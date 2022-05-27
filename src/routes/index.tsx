fab45a8013418106ea15c9b2e59d61b5e512bc60
mcristiano <machado.cristiano@gmail.com>
2022-05-27 15:09:57 +0000

    feat: added react-router-dom

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,15 @@
-export { };
+import { Routes, Route, Navigate } from 'react-router-dom'
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path='/initial-page' element={ <p>Pagina Inicial</p> }  />
+
+
+      <Route path='*' element={<Navigate to="/initial-page" />}  />
+
+    </Routes>
+
+  );
+
+}
