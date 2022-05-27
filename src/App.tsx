181a71356b16b33dee9b058c508b1e76de7f36bd
mcristiano <machado.cristiano@gmail.com>
2022-05-27 16:06:20 +0000

    feat: Added controle de temas e definido o tema claro

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
+import { ThemeProvider } from "@emotion/react";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from './routes/index';
+import { LightTheme } from './shared/themes/light';
 
 
 export const App = () => {
   return (
-  <BrowserRouter>
-      <AppRoutes />      
-  </BrowserRouter>
-
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+          <AppRoutes />      
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
+import { Button } from '@mui/material';
 import { Routes, Route, Navigate } from 'react-router-dom'
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path='/initial-page' element={ <p>Pagina Inicial</p> }  />
+      <Route path='/initial-page' element={ <Button variant='contained' color='primary'  >TESTE</Button>  }  />
 
 
       <Route path='*' element={<Navigate to="/initial-page" />}  />
